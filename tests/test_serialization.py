1459f201addeb5688613715d835bed9929662764
Chris Heisel <chris@heisel.org>
2009-03-31 15:25:25 -0400

    Refactored serialization tests.

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -22,21 +22,27 @@ class SerializationTestHelper(unittest.TestCase):
 
         self.project.bc.load_test_fixtures(self.fixtures_path)
 
-class MessageSerializationTests(SerializationTestHelper):
-    def test_serialization(self):
-        m = self.project.messages[0]
-        as_json = m.to_json()
-
+    def assertSerialization(self, o, expected):
+        as_json = o.to_json()
         from_json = json.loads(as_json)
-        expected = {
-            "category": {"type": "PostCategory", "id": 28605393, "name": "Assets"},
-            "attachments_count": 0,
-            "id": 19364228,
-            "posted_on": '2009-01-28T14:30:18',
-            "title": "This is the newest message"
-        }
 
-        self.assertEqual(expected, from_json)
+        msg = "Unequal data structures!\nExpected:\n%s\n\nActual:\n%s" % (pprint.pformat(expected), pprint.pformat(from_json))
+        self.assertEqual(expected, from_json, msg)
+
+class SerializationTests(SerializationTestHelper):
+    def test_message(self):
+        m = self.project.messages[0]
+        expected = {
+            u"category": {u"type": u"PostCategory", u"id": 28605393, u"name": u"Assets"},
+            u"attachments_count": 0,
+            u"id": 19364228,
+            u"posted_on": u'2009-01-28T14:30:18',
+            u"title": u"This is the newest message"
+        }
+        self.assertSerialization(m, expected)
+
+    def test_comment(self):
+        pass
 
 if __name__ == "__main__":
     import unittest
